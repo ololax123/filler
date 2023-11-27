3d2b022b9e3dc59ebdde742669797bf3fa28854a
goran@rundberg.ax <goran@rundberg.ax>
2023-11-27 16:56:34 +0200

    english damnit

--- src/fransfan.rs
+++ src/fransfan.rs
@@ -1,29 +1,29 @@
-// Cette fonction calcule la distance la plus courte entre un caractère spécifié et un autre caractère spécifié.
+// Calculate the shortest distance to the oponent.
 pub fn short_distance(
     grid: &Vec<Vec<char>>,
     distance: f32,
     play: &Vec<char>,
     enemy: &Vec<char>,
 ) -> f32 {
-    let mut min_dist = distance; // Cette variable stocke la distance minimale trouvée.
+    let mut min_dist = distance;
 
-    // Parcourir chaque ligne de la grille.
+    // Iterate rows.
     for (yg, row) in grid.iter().enumerate() {
-        // Parcourir chaque colonne de la ligne actuelle.
+        // iterate columns in row
         for (xg, &cell) in row.iter().enumerate() {
-            // Vérifier si le caractère actuel correspond au deuxième caractère dans `play`.
+            // Check if we're in the cell.
             if cell == play[1] {
-                // Une autre boucle pour chaque ligne de la grille pour comparer avec `enemy`.
+                // Then go for the oponent.
                 for (ye, enemy_row) in grid.iter().enumerate() {
-                    // Boucle sur chaque colonne de la ligne courante pour cette deuxième boucle.
+                    // check all chars in the row.
                     for (xe, &enemy_cell) in enemy_row.iter().enumerate() {
-                        // Vérifier si le caractère à cette position est dans `enemy`.
+                        // Did we dind the enemy?`.
                         if enemy.contains(&enemy_cell) {
-                            // Calcul de la distance euclidienne entre (yg, xg) et (ye, xe) en utilisant le théorème de Pythagore.
+                            // Make some maths and find the distnace #Pythagoras
                             let dist = (((ye as f32) - (yg as f32)).powf(2.)
                                 + ((xe as f32) - (xg as f32)).powf(2.))
                             .sqrt();
-                            // Si la distance calculée est inférieure à `min_dist`, la mettre à jour.
+                            // If it's closer, save it.
                             min_dist = min_dist.min(dist);
                         }
                     }
@@ -31,31 +31,27 @@ pub fn short_distance(
             }
         }
     }
-    // Retourner la distance minimale trouvée.
+    // Return the perfect solution
     min_dist
 }
 
-/****************************************************\
-    Fonction pour les meilleures coordonnées pour
-    placer une pièce sur la grille.
-\****************************************************/
 pub fn place_piece(
-    grid: &Vec<Vec<char>>,  // La grille du jeu
-    piece: &Vec<Vec<char>>, // La pièce à placer
-    play: &Vec<char>,       // Caractères valides pour le placement
-    enemy: &Vec<char>,      // Caractères adverses ou non valides
+    grid: &Vec<Vec<char>>,
+    piece: &Vec<Vec<char>>,
+    play: &Vec<char>,
+    enemy: &Vec<char>,
 ) -> (usize, usize) {
-    // Initialisation des variables et calcul de la distance initiale
+    // Get everything needed to calculate the distance.
     let grid_rows = grid[0].len();
     let piece_rows = piece[0].len();
-    let mut distance = ((grid_rows as f32).powf(2.) + (grid.len() as f32).powf(2.)).sqrt(); //powf(2.) = puissance 2(multiplication par lui-même)
+    let mut distance = ((grid_rows as f32).powf(2.) + (grid.len() as f32).powf(2.)).sqrt(); // Max distance
     let mut sol = (0, 0);
 
-    // Calcul des limites basées sur les caractères valides dans play
+    //  Create a copy of the grid for modification
     let (mut xmin, mut xmax, mut ymin, mut ymax) = (grid.len(), 0, grid_rows, 0);
     for yg in 0..grid.len() {
         for xg in 0..grid_rows {
-            // Si la cellule actuelle contient un caractère valide pour le jeu, ajuste les limites.
+            // Replace opponent characters with valid characters.
             if play.contains(&grid[yg][xg]) {
                 xmin = xmin.min(xg);
                 xmax = xmax.max(xg);
@@ -65,7 +61,7 @@ pub fn place_piece(
         }
     }
 
-    // Ajustement de la plage de recherche pour le placement de la pièce
+    // Set the limits for the piece placement.
     let (mut xi, mut xf, mut yi, mut yf) = (
         0,
         grid_rows - piece_rows + 1,
@@ -73,7 +69,7 @@ pub fn place_piece(
         grid.len() - piece.len() + 1,
     );
 
-    // Ajustement des limites pour le placement de la pièce
+    // adjust the limits if needed.
     let temp = xmin as i32 - piece_rows as i32 - 1;
     if temp > 0 {
         xi = xmin - piece_rows + 1
@@ -89,7 +85,7 @@ pub fn place_piece(
         yf = ymax + 1
     }
 
-    // Recherche chaque cellule potentielle de la grille pour le placement de la pièce.
+    // verify all the possible positions.
     for yg in yi..yf {
         for xg in xi..xf {
             if put_piece(grid, piece, play, xg, yg) {
@@ -106,25 +102,23 @@ pub fn place_piece(
             }
         }
     }
-    // Retourne les meilleures coordonnées trouvées
+    // Return the best solution.
     sol
 }
-///////////////////////// FIN ////////////////////////////////
 
-/****************************************************\
- Fonction pour placer une pièce sur la grille existante.
-\****************************************************/
+// Function to place a piece on a grid.
+
 pub fn grid_with_piece(
-    grid: &[Vec<char>],     // La grille du jeu actuelle
-    piece: &[Vec<char>],    // La pièce à placer
-    play: &[char],          // Caractères spécifiques pour certaines actions
-    coords: (usize, usize), // Coordonnées pour placer la pièce
+    grid: &[Vec<char>],
+    piece: &[Vec<char>],
+    play: &[char],
+    coords: (usize, usize),
 ) -> Vec<Vec<char>> {
-    // Création d'une copie de la grille pour la modification
+    // Create a modifyable copy
     let mut new_grid = grid.to_vec();
     let prows = piece[0].len(); // number of piece rows
 
-    // Remplacement des caractères adverses par les caractères valides
+    // Replace with valid chars
     for yg in 0..new_grid.len() {
         for xg in 0..new_grid[0].len() {
             if new_grid[yg][xg] == play[1] {
@@ -142,44 +136,37 @@ pub fn grid_with_piece(
     }
     new_grid
 }
-///////////////////////// FIN //////////////////////////////
 
-/****************************************************\
-    Fonction pour placer une pièce sur une grille.
-\****************************************************/
+//Function to place the piece
 pub fn put_piece(
-    grid: &[Vec<char>],  // La grille du jeu
-    piece: &[Vec<char>], // La pièce à placer
-    play: &[char],       // Caractères valides pour le placement
-    xg: usize,           // Coordonnée X sur la grille pour le placement
-    yg: usize,           // Coordonnée Y sur la grille pour le placement
+    grid: &[Vec<char>],
+    piece: &[Vec<char>],
+    play: &[char],
+    xg: usize,
+    yg: usize,
 ) -> bool {
-    let mut cross = 0; // Compteur pour les intersections valides
+    let mut cross = 0; // Counter for valid intersections
 
-    // Parcourir chaque ligne (row) et colonne (val) de la pièce
+    // Go through each line (row) and column (val) of the piece
     for (yp, row) in piece.iter().enumerate() {
         for (xp, &val) in row.iter().enumerate() {
-            // Traitement seulement pour les caractères non vides
+            // Check if the cell is part of the piece
             if val != '.' {
-                // Obtention du caractère correspondant sur la grille
                 let cell = grid.get(yg + yp).and_then(|r| r.get(xg + xp));
 
                 match cell {
-                    // Incrémentation de cross si le caractère est valide
                     Some(&c) if play.contains(&c) => cross += 1,
-                    // Retour immédiat de false si un caractère non valide est trouvé
                     Some(&c) if c != '.' => return false,
                     _ => {}
                 }
 
-                // Retour de false si plus d'une intersection valide est trouvée
+                // Return false if more than one valid intersection is found
                 if cross > 1 {
                     return false;
                 }
             }
         }
     }
-    // Retourne true si exactement une intersection valide est trouvée
+    // Return true if exactly one valid intersection is found
     cross == 1
 }
-///////////////////////// FIN ////////////////////////////
